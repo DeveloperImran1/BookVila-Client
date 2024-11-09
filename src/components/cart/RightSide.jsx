c95313d27283adf5ab3202b916d0fa9ee682100f
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-11-09 12:38:49 +0600

    PromoCode input feld  aadd

--- src/components/cart/RightSide.jsx
+++ src/components/cart/RightSide.jsx
@@ -1,11 +1,13 @@
 import DiscountCard from "./DiscountCard";
 import OrderSummary from "./OrderSummary";
+import PromoCode from "./PromoCode";
 
 const RightSide = () => {
     return (
         <div>
             <OrderSummary/>
             <DiscountCard/>
+            <PromoCode/>
         </div>
     );
 };
