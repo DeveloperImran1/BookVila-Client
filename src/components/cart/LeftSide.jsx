934a86451f02b389377bea15293cd39b72f57f9c
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-11-09 12:23:50 +0600

    Create Left Side Table

--- src/components/cart/LeftSide.jsx
+++ src/components/cart/LeftSide.jsx
@@ -1,8 +1,17 @@
 
 const LeftSide = () => {
     return (
-        <div>
-            LeftSide
+        <div className='bg-white w-full p-4 sm:p-6 md:p-7 lg:p-10'>
+            <h1 className="text-sm sm:text-xl font-inter font-semibold">My cart</h1>
+            <div className="mt-3 sm:mt-5 md:mt-6 lg:mt-8 overflow-scroll">
+                <table className='table table-px-0 border-t'>
+                    <tbody>
+                        {[1, 2,1,1,1,1,1,].map((item, index) => (
+                            <tr key={index.index}>ff</tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
